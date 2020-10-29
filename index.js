35197550e4dc1a8c55effa5472c0a015b2230758
Ana Tulea <anatulea@Anas-MacBook-Pro.local>
2020-10-29 00:35:49 -0700

    fixed error

--- index.js
+++ index.js
@@ -106,5 +106,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ type: 'error', message: err.message });
 });
 
-const PORT = port.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
